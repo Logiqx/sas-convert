224a89adfb25b3603f199f5216db58fd5ff6f177
Michael George <michael.george@directlinegroup.co.uk>
2016-07-20 14:43:45 +0100

    Enhanced sas-convert to support stdout + additional options
    Output filename is now optional, allowing output to be streamed through stdout.
    
    Additional options:
    --columns    List Columns
    --limit <n>   Limit records
    --info            Summary Information
    --test            Test only
    
    Note: SAS file properties are not displayed during the extract to prevent them being mixed up with actual data.

--- src/main/java/thehyve/sas/Convert.java
+++ src/main/java/thehyve/sas/Convert.java
@@ -19,6 +19,7 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.HelpFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,39 +40,27 @@ import com.epam.parso.impl.SasFileReaderImpl;
  */
 public class Convert {
 
-    public static final String USAGE = "Usage: Convert <file.sas> <file.csv>";
+    public static final String USAGE = "sas-convert-0.2.jar [option] file.sas [file.csv]";
     Logger log = LoggerFactory.getLogger(getClass());
 
-    public void convert(InputStream in, OutputStream out) throws IOException {
-        Date start = new Date();
+    public void convert(InputStream in, OutputStream out, int max) throws IOException {
         SasFileReader reader = new SasFileReaderImpl(in);
         CSVWriter writer = new CSVWriter(new OutputStreamWriter(out));
         Object[] data;
-        SasFileProperties properties = reader.getSasFileProperties();
-        log.info("Reading file " + properties.getName());
-        log.info(properties.getRowCount() + " rows.");
         List<Column> columns = reader.getColumns();
         String[] outData = new String[columns.size()];
-        // Writing column labels
-        for(int i=0; i < columns.size(); i++) {
-            outData[i] = columns.get(i).getLabel();
-        }
-        writer.writeNext(outData);
         // Writing column names
         for(int i=0; i < columns.size(); i++) {
             outData[i] = columns.get(i).getName();
         }
         writer.writeNext(outData);
-        // Writing column format
-        for(int i=0; i < columns.size(); i++) {
-            outData[i] = columns.get(i).getFormat();
-        }
-        writer.writeNext(outData);
 
         try {
-            log.info("Writing data...");
             long rowCount = 0;
             while((data = reader.readNext()) != null) {
+                if (max != -1 && rowCount >= max) {
+                    break;
+                }
                 assert(columns.size() == data.length);
                 for(int i=0; i < data.length; i++) {
                     outData[i] = data[i] == null ? "" : data[i].toString();
@@ -79,47 +68,148 @@ public class Convert {
                 writer.writeNext(outData);
                 rowCount++;
             }
-            log.info("Done writing data.");
-            log.info(rowCount + " rows written.");
             writer.flush();
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
             throw e;
         }
+    }
+    
+    public void test(InputStream in) throws IOException {
+        Date start = new Date();
+        SasFileReader reader = new SasFileReaderImpl(in);
+        Object[] data;
+        SasFileProperties properties = reader.getSasFileProperties();
+        log.info("Reading file " + properties.getName());
+        log.info(properties.getRowCount() + " rows.");
+        List<Column> columns = reader.getColumns();
+        String[] outData = new String[columns.size()];
+
+        try {
+            log.info("Testing data...");
+            long rowCount = 0;
+            while((data = reader.readNext()) != null) {
+                assert(columns.size() == data.length);
+                rowCount++;
+            }
+            log.info("Done testing data.");
+            log.info(rowCount + " rows tested.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
         Date end = new Date();
-        log.info("Converting took {} seconds.", (end.getTime() - start.getTime())/1000);
+        log.info("Testing took {} seconds.", (end.getTime() - start.getTime())/1000);
+    }
+    
+    public void columns(InputStream in, OutputStream out) throws IOException {
+        SasFileReader reader = new SasFileReaderImpl(in);
+        CSVWriter writer = new CSVWriter(new OutputStreamWriter(out));
+        Object[] data;
+        List<Column> columns = reader.getColumns();
+        String[] outData = new String[6];
+        // Writing column names
+        outData[0] = "Id";
+        outData[1] = "Name";
+        outData[2] = "Label";
+        outData[3] = "Format";
+        outData[4] = "Type";
+        outData[5] = "Length";
+        writer.writeNext(outData);
+
+        try {
+            for(int i=0; i < columns.size(); i++) {
+                outData[0] = Integer.toString(columns.get(i).getId());
+                outData[1] = columns.get(i).getName();
+                outData[2] = columns.get(i).getLabel();
+                outData[3] = columns.get(i).getFormat();
+                outData[4] = columns.get(i).getType().getSimpleName();
+                outData[5] = Integer.toString(columns.get(i).getLength());
+                writer.writeNext(outData);
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+    
+    public void info(InputStream in) throws IOException {
+        SasFileReader reader = new SasFileReaderImpl(in);
+        SasFileProperties properties = reader.getSasFileProperties();
+        log.info("Reading file " + properties.getName());
+        log.info(properties.getRowCount() + " rows.");
+
+        List<Column> columns = reader.getColumns();
+        log.info(columns.size() + " columns.");
     }
     
     public static void main(String[] args) {
         Options options = new Options();
+        options.addOption("c", "columns", false, "List Columns");
         options.addOption("h", "help", false, "Help");
+        options.addOption("l", "limit", true, "Limit Records");
+        options.addOption("i", "info", false, "Summary Information");
+        options.addOption("t", "test", false, "Test Only");
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine cl = parser.parse(options, args);
             if (cl.hasOption("help")) {
-                System.out.printf(USAGE + "\n");
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp(USAGE + "\n", options);
                 return;
             }
             List<String> argList = cl.getArgList();
-            if (argList.size() < 2) {
-                System.out.printf("Too few parameters.\n" + USAGE + "\n");
+            if (argList.size() < 1) {
+                System.err.printf("Too few parameters.\n");
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp(USAGE + "\n", options);
                 return;
             }
             try {
                 FileInputStream fin = new FileInputStream(argList.get(0));
-                FileOutputStream fout = new FileOutputStream(argList.get(1));
                 Convert converter = new Convert();
-                converter.convert(fin, fout);
+                if (cl.hasOption("info")) {
+                    converter.info(fin);
+                }
+                else if (cl.hasOption("test")) {
+                    converter.test(fin);
+                }
+                else if (cl.hasOption("columns")) {
+                    if (argList.size() == 1) {
+                        converter.columns(fin, System.out);
+                    }
+                    else {
+                        FileOutputStream fout = new FileOutputStream(argList.get(1));
+                        converter.columns(fin, fout);
+                        fout.close();
+                    }
+                }
+                else {
+                    int max = -1;                 
+                    if (cl.hasOption("limit")) {
+                        max = Integer.parseInt(cl.getOptionValue("limit"));                        
+                    }
+                    if (argList.size() == 1) {
+                        converter.convert(fin, System.out, max);
+                    }
+                    else {
+                        FileOutputStream fout = new FileOutputStream(argList.get(1));
+                        converter.convert(fin, fout, max);
+                        fout.close();
+                    }
+                }
                 fin.close();
-                fout.close();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } catch (ParseException e) {
-            System.out.printf(USAGE + "\n");
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(USAGE + "\n", options);
             e.printStackTrace();
             return;
         }
